3bde3739ec2891f00a3b1a1b39ec438a5acbfc53
libra189 <libra189@gmail.com>
2020-07-10 16:33:11 +0900

    Project init

---
+++ index.rb
@@ -0,0 +1,14 @@
+#! /usr/bin/ruby
+# エントリーポイント
+
+# 処理要件
+# ユーザのデータをjsonファイルから読み込み
+# 利用しやすいようにデータをstructに格納
+# ID:1とID:2の年齢差を求める
+
+# 全ライブラリ読み込み
+require 'bundler/setup'
+Bundler.require
+
+puts 'Start processing.'
+puts "\nFinish processing."
