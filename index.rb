83ed18034f28f097379d814a33da243ad76c83e3
libra189 <libra189@gmail.com>
2020-07-19 01:15:14 +0900

    add infrastructure layer インフラ層を追加

--- index.rb
+++ index.rb
@@ -2,29 +2,34 @@
 
 # 処理要件
 # ユーザのデータをjsonファイルから読み込み
-# 利用しやすいようにデータをstructに格納
+# 利用しやすいようにデータをmodelに格納
 # ID:1とID:2の年齢差を求める
 
 # 全ライブラリ読み込み
 require 'bundler/setup'
 Bundler.require
 
-# domein/model読み込み
-require "#{File.expand_path(__dir__)}/src/domain/model/user"
-require "#{File.expand_path(__dir__)}/src/domain/repository/user"
+# インフラ層の読み込み
+require "#{File.expand_path(__dir__)}/src/infrastructure/user"
 
 puts "Start processing.\n\n"
 
+# DI
+# インフラ層の初期化
+user_repository = Infrastructure::User.new("#{File.expand_path(__dir__)}/json/User.json")
+
+# インフラ層からデータを読み込み
 # @params [Model::User] user1
-user1 = Repository::User.find(1)
+user1 = user_repository.find(1)
 puts 'load user data at id:1.'
 puts user1
 
 # @params [Model::User] user1
-user2 = Repository::User.find(2)
+user2 = user_repository.find(2)
 puts 'load user data at id:2.'
 puts user2
 
+# ドメイン層でビジネスロジック呼び出し
 puts "\nCalculate age difference to ID:1 from ID:2."
 puts "id:1 - id:2 = #{user1.diff_age(user2)}"
 
