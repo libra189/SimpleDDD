8ef52b737559e5e731385cad92e1cf1d680b9e0c
libra189 <libra189@gmail.com>
2020-08-11 23:08:17 +0900

    add #6 user usecase skeleton code on method Userユースケースのメソッドのスケルトンコードを追加

--- src/usecase/user.rb
+++ src/usecase/user.rb
@@ -4,7 +4,28 @@ module Usecase
   require "#{File.expand_path(__dir__)}/../domain/model/user"
   require "#{File.expand_path(__dir__)}/../domain/repository/user"
 
+  # User usercase class
   class User
+    # @params [String] name
+    # @params [Date] birthday
+    # @params [String] gender
+    # @return [Model::User]
+    def create(name, birthday, gender)
+    end
 
+    # @params [Integer] id
+    # @return [Model::User]
+    def find_by_id(id)
+    end
+
+    # @params [Model::User] user
+    # @return [Model::User]
+    def update(user)
+    end
+
+    # @params [Integer] id
+    # @return [Integer]
+    def delete(id)
+    end
   end
 end
